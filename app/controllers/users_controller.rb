c242cf460033baabe0f3dace2c96f293840f1243
sammiepls <samantha.lhe@gmail.com>
2017-09-15 17:41:53 +0800

    Create basic layout and styling for users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < Clearance::UsersController
 
   private
   def user_params
-    params.require(:user).permit(:first_name,:last_name,:email,:password)
+    params.require(:user).permit(:first_name,:last_name,:email,:password,:created_at,:updated_at)
   end
 
 end
--- db/migrate/20170915022039_create_users.rb
+++ db/migrate/20170915022039_create_users.rb
@@ -8,6 +8,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :encrypted_password, limit: 128, null: false
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
+      t.timestamps 
     end
 
     add_index :users, :email
