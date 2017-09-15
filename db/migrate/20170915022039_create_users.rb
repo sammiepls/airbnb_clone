bfa58a866913f00752eacc60a5fafeca064db755
sammiepls <samantha.lhe@gmail.com>
2017-09-15 10:58:41 +0800

    Run users/clearance migration

--- db/migrate/20170915022039_create_users.rb
+++ db/migrate/20170915022039_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.timestamps null: false
       t.string :email, null: false
+      t.string :first_name
+      t.string :last_name
       t.string :encrypted_password, limit: 128, null: false
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
