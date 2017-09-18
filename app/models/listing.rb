8232db7a8f144d3f20095723e3110dbd3f83f544
sammiepls <samantha.lhe@gmail.com>
2017-09-18 14:06:50 +0800

    Create listings

---
+++ app/models/listing.rb
@@ -0,0 +1,10 @@
+class Listing < ApplicationRecord
+  # Validation
+  NON_VALIDATABLE_ATTRS = ["id", "created_at", "updated_at", "user_id"]
+  #^or any other attribute that does not need validation
+  VALIDATABLE_ATTRS = Listing.attribute_names.reject{|attr| NON_VALIDATABLE_ATTRS.include?(attr)}
+  validates_presence_of VALIDATABLE_ATTRS
+  #validating the presence of everything else
+  validates :price_per_night, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: true
+
+end
