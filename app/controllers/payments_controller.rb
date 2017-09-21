21e8c5b6654ea4ef4e218af2b462e4825c3cce7d
sammiepls <samantha.lhe@gmail.com>
2017-09-21 18:10:02 +0800

    Payment feature implemented

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,4 +2,24 @@ class PaymentsController < ApplicationController
   def new
     @client_token = Braintree::ClientToken.generate
   end
+
+  def checkout
+    nonce_from_the_client = params[:checkout_form][:payment_method_nonce]
+     result = Braintree::Transaction.sale(
+      :amount => "10.00", #this is currently hardcoded
+      :payment_method_nonce => nonce_from_the_client,
+      :options => {
+         :submit_for_settlement => true
+       }
+      )
+
+     if result.success?
+       byebug
+       flash[:success] = "Your transaction was successful!"
+       redirect_to :root
+     else
+       flash[:failure] = "There was an error in your transaction. Please try again"
+       redirect_to :root
+     end
+  end
 end
