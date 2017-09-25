31309b8bd9481a03ef49cd8cc9700be94c674201
sammiepls <samantha.lhe@gmail.com>
2017-09-25 19:02:08 +0800

    Reset host name for mailer

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -4,7 +4,7 @@ class ReservationMailer < ApplicationMailer
   def reservation_confirmation_email(customer,reservation)
     @reservation = reservation
     @customer = customer
-    @url = user_reservation_url(customer, @reservation, host: 'http://localhost:3000')
+    @url = user_reservation_url(customer, @reservation, host: 'https://tranquil-lake-88359.herokuapp.com')
     mail(to: @customer.email, subject: 'Your reservation is confirmed')
   end
 end
